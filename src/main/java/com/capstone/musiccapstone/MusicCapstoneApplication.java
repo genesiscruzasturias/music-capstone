0a156a336fd0b448125ac2b200150f29cd049d03
Genesis Cruz <genesis.cruzasturias@gmail.com>
2024-01-15 15:17:21 -0600

    adding .gitignore

---
+++ src/main/java/com/capstone/musiccapstone/MusicCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.musiccapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicCapstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/musiccapstone/MusicCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.musiccapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
