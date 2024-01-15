2f86fefcb2035600a8fd7698dbe314573d07b971
Genesis Cruz <genesis.cruzasturias@gmail.com>
2024-01-15 15:18:18 -0600

    commit application.properties

--- src/main/java/com/capstone/musiccapstone/MusicCapstoneApplication.java
+++ src/main/java/com/capstone/musiccapstone/MusicCapstoneApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MusicCapstoneApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MusicCapstoneApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MusicCapstoneApplication.class, args);
+    }
 
 }
